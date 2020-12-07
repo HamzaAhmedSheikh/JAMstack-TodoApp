8534153911cce246bc06a57af46e1486fbc27c1a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-12-07 23:12:15 +0500

    updates

--- packages/www/.cache/sync-requires.js
+++ packages/www/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("F:\\JAMstack Serverless and Headless CMS\\JAMstack-TodoApp\\packages\\www\\.cache\\dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("F:\\JAMstack Serverless and Headless CMS\\JAMstack-TodoApp\\packages\\www\\src\\pages\\index.js")))
 }
 
