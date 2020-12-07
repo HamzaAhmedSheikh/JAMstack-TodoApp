2c5c49354ef412a2132f182a0a6ddfacd6676ada
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-12-07 22:49:15 +0500

    Gatsby Hello World
    Basic Gatsby Hello World App

---
+++ packages/www/.cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("F:\\JAMstack Serverless and Headless CMS\\JAMstack-TodoApp\\packages\\www\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("F:\\JAMstack Serverless and Headless CMS\\JAMstack-TodoApp\\packages\\www\\src\\pages\\index.js")))
+}
+
